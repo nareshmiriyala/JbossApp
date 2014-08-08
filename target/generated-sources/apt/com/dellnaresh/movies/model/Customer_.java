a2a14f1c49b866c5569e581c0ac0c9594776f982
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2014-08-08 17:38:09 +1000

    commit

---
+++ target/generated-sources/apt/com/dellnaresh/movies/model/Customer_.java
@@ -0,0 +1,15 @@
+package com.dellnaresh.movies.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Customer.class)
+public abstract class Customer_ {
+
+	public static volatile SingularAttribute<Customer, Long> id;
+	public static volatile SingularAttribute<Customer, Integer> version;
+
+}
+
